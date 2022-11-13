16e3478611da4f2c9568ac33b5b76d9dbcc1e49f
Rais Alam <rais7jmi@gmail.com>
2022-11-13 18:02:56 +0530

    Empty branch

--- src/main/java/com/springboot/HREMS/repository/IEmployerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.HREMS.repository;
-
-import com.springboot.HREMS.models.Employers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IEmployerRepository extends JpaRepository<Employers,String> {
-    Employers findByEmailId(String emailId);
-}
--- src/main/java/com/springboot/HREMS/services/IPaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.HREMS.services;
-
-import com.razorpay.RazorpayException;
-import com.springboot.HREMS.payloads.ApiResponse;
-import com.springboot.HREMS.payloads.OrdersDtoRequest;
-
-public interface IPaymentService {
-    ApiResponse createOrder(OrdersDtoRequest request) throws RazorpayException;
-}
