46417d12e3d342e933fd55a53ddc54539ff93702
Anshul Kumar <anshul.kumar04@sap.com>
2022-11-26 12:17:17 +0530

    Code Refactoring

--- src/main/java/com/safehiring/ems/api/EmployerAPI.java
+++ src/main/java/com/safehiring/ems/api/EmployerAPI.java
@@ -1,9 +1,14 @@
 package com.safehiring.ems.api;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,5 +21,10 @@ public interface EmployerAPI {
 	@PostMapping(value = "/employmentoffer", produces = { "application/json" }, consumes = { "application/json" })
 	ResponseEntity<JobOfferResponse> createJobOffers(@RequestBody @Valid final JobOfferRequest jobOfferRequest);
 
+	@GetMapping(value = "/employmentoffer/{tin}")
+	ResponseEntity<List<JobOfferResponse>> getAllEmploymentOffers(@PathVariable final String tin);
+
+	@PutMapping(value = "/employmentoffer", produces = { "application/json" }, consumes = { "application/json" })
+	ResponseEntity<JobOfferResponse> updateEmploymentOffers(@RequestBody @Valid final JobOfferRequest jobOfferRequest);
 
 }
--- src/main/java/com/safehiring/ems/controller/EmployerController.java
+++ src/main/java/com/safehiring/ems/controller/EmployerController.java
@@ -1,5 +1,8 @@
 package com.safehiring.ems.controller;
 
+import java.util.Collections;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -7,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.safehiring.ems.api.EmployerAPI;
+import com.safehiring.ems.exception.InvalidJobOfferException;
 import com.safehiring.ems.model.request.JobOfferRequest;
 import com.safehiring.ems.model.response.JobOfferResponse;
 import com.safehiring.ems.service.JobOfferService;
@@ -17,7 +21,8 @@ import lombok.extern.slf4j.Slf4j;
 @CrossOrigin("*")
 @Data
 @Slf4j
-//TO DO Protect This controller with Scope= Employer meaning only an employer can access this API
+//TO DO
+// Protect This controller with Scope= Employer meaning only an employer can access this API
 //@RequiresScope({ Scope.Employer })
 public class EmployerController implements EmployerAPI {
 
@@ -27,5 +32,23 @@ public class EmployerController implements EmployerAPI {
 	public ResponseEntity<JobOfferResponse> createJobOffers(@Valid final JobOfferRequest jobOfferRequest) {
 		return new ResponseEntity<>(this.jobOfferService.saveJobOffer(jobOfferRequest), HttpStatus.CREATED);
 	}
-	
+
+	@Override
+	public ResponseEntity<List<JobOfferResponse>> getAllEmploymentOffers(final String tin) {
+		try {
+			return new ResponseEntity<>(this.jobOfferService.getAllJobsByTin(tin), HttpStatus.OK);
+		} catch (final InvalidJobOfferException e) {
+			return new ResponseEntity<>(Collections.EMPTY_LIST, HttpStatus.NO_CONTENT);
+		}
+	}
+
+	@Override
+	public ResponseEntity<JobOfferResponse> updateEmploymentOffers(@Valid final JobOfferRequest jobOfferRequest) {
+		//TO DO
+		//1. An Employer can update only his own offer letters.
+		//2. Fetch the respective employers offers & then search for Job offer provided by Tin
+		//3. Update the respective fields.
+		//4. An Employer can update only:- "Employment Status".
+		return null;
+	}
 }
